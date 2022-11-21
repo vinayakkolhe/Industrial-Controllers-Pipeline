9f26a6766879acf6bf72d890053922213a2e4689
vinayakkolhe <61221205+vinayakkolhe@users.noreply.github.com>
2022-11-21 16:54:51 +0530

    2.1.4/H

--- src/app/app.py
+++ src/app/app.py
@@ -1,2 +1,17 @@
-class main:
-    name = "Vinayak Kolhe"
+import time
+import sys
+
+class pyapp:
+
+    def __init__(self):
+        self.on_time = 4  #in sec
+        self.off_time = 4 #in sec
+
+    def main(self):
+        sys.stdout.write('\r....No' )
+        sys.stdout.flush()
+        time.sleep(self.on_time)
+        
+        sys.stdout.write('\r...Off' )
+        sys.stdout.flush()
+        time.sleep(self.off_time)
