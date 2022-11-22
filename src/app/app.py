a0479d39a96f94bd9985fa75768ac7391f2c2a8a
vinayakkolhe <61221205+vinayakkolhe@users.noreply.github.com>
2022-11-22 17:31:44 +0530

    2.1.12/H

--- src/app/app.py
+++ src/app/app.py
@@ -1,17 +1,34 @@
 import time
 import sys
+from colorama import init, Fore, Back, Style
+import click
+init(convert=True)
+
 
 class pyapp:
 
+
     def __init__(self):
+        
+    
         self.on_time = 1  #in sec
         self.off_time = 3 #in sec
+        self.lamp_color = "yellow"
 
     def main(self):
-        sys.stdout.write('\r....On' )
+        """
+        This is main function which is getting executed in runner in loop.
+        """
+        sys.stdout.write('\r....' + click.style('On', self.lamp_color))
         sys.stdout.flush()
         time.sleep(self.on_time)
         
-        sys.stdout.write('\r...Off' )
+        sys.stdout.write('\r...' + click.style('Off', self.lamp_color))
         sys.stdout.flush()
         time.sleep(self.off_time)
+        
+        
+
+    
+if __name__ == "__main__":
+    pyapp().main()
