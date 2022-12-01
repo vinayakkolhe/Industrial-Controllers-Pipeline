6557c8725a471d7d74d4cfd1ec496006b17dcda7
vinayakkolhe <61221205+vinayakkolhe@users.noreply.github.com>
2022-12-01 18:28:33 +0530

    2.2.10/H

---
+++ src/app/app.py
@@ -0,0 +1,22 @@
+import os
+import time
+
+class pyapp:
+    def __init__(self):
+        self.ledNumber = 1
+        self.led = str(self.ledNumber)
+        self.on_time = 1
+        self.off_time = 1
+        os.popen("sudo sh -c 'echo none > /sys/class/leds/led"+self.led+"/trigger'")
+        
+
+    def main(self):
+        os.popen("sudo sh -c 'echo 1 > /sys/class/leds/led"+self.led+"/brightness'")
+        time.sleep(self.on_time)
+        os.popen("sudo sh -c 'echo 0 > /sys/class/leds/led"+self.led+"/brightness'")
+        time.sleep(self.off_time)
+    
+    
+if __name__ == "__main__":
+    app = pyapp()
+    app.main()
